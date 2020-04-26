27b1a454acf332772e7e7c0265718cff3c20c640
NihithReddy <sainihith9618@gmail.com>
2020-04-26 23:05:10 +0530

    Added code to fetch images

--- app/src/main/java/com/example/travelblog/MainActivity.java
+++ app/src/main/java/com/example/travelblog/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.travelblog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView mainTextView = findViewById(R.id.mainTextView);
+        mainTextView.setText("Hello educative.io");
+
+        startActivity(new Intent(this, BlogActivity.class));
     }
 }
