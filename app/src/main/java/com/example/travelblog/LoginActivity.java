6b3311091d7f1265b790968ab438b9ca9601f848
NihithReddy <sainihith9618@gmail.com>
2020-04-26 19:41:42 +0530

    LoginActivity file added

---
+++ app/src/main/java/com/example/travelblog/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.travelblog;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.travelblog.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_login);
+    }
+}
